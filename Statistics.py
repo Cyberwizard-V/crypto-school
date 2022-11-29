23774567d711787eca1e97b960b3405329088292
BMO | Viktor <veke2003@outlook.com>
2022-11-29 15:32:43 +0100

    MIN, MAX, SD, Q1-Q3, IQR

--- Statistics.py
+++ Statistics.py
@@ -10,7 +10,7 @@ class Statistics:
     def __init__(self) -> None:
         self.api = c_api.Api()
         self.allCoinData = self.getAllCoinData()
-        self.statisticData = {"AVG": [], "MIN": [], "MAX": [], "SD": [], "Q1": [], "Q2": [], "Q3": [], "RNG": [], "IQR": [], "UPS": [], "DOWN": [], "LUP": [], "LDWN": [],}
+        self.statisticData = {"AVG": [], "MIN": [], "MAX": [], "SD": [], "Q1": [], "Q2": [], "Q3": [],"IQR": [], "UPS": [], "DOWN": [], "LUP": [], "LDWN": [],}
 
         # create statistics
         self.getStatistics()
@@ -24,9 +24,27 @@ class Statistics:
     def getStatistics(self):
         for x in range(0, len(self.allCoinData)):
             coinData = [val["value"] for val in self.allCoinData[x]["history"]]
+            coinSD = np.array([coinData])
 
+            # First quartile (Q1)
+            Q1 = np.percentile(coinData, 25)
+            # Second quartile (Q2)
+            Q2 = np.percentile(coinData, 50)
+            # Third quartile (Q3)
+            Q3 = np.percentile(coinData, 75)
+            # Interquaritle range (IQR)
+            IQR = Q3 - Q1
             # append coindata
             self.statisticData["AVG"].append(self.calculateAverage(coinData))
+            self.statisticData["MIN"].append(self.calculateAverage(min(coinData)))
+            self.statisticData["MAX"].append(self.calculateAverage(max(coinData)))
+            self.statisticData["SD"].append(self.calculateAverage(np.std(coinSD)))
+            self.statisticData["Q1"].append(self.calculateAverage(Q1))
+            self.statisticData["Q2"].append(self.calculateAverage(Q2))
+            self.statisticData["Q3"].append(self.calculateAverage(Q3))
+            self.statisticData["IQR"].append(self.calculateAverage(IQR))
+
+
 
     def calculateAverage(self, coinData: list):
         return np.average(coinData)
