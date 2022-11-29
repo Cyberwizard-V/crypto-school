0b0a75898b375e991ab667641f40579868dce236
Kroonsz <Kroonszq@gmail.com>
2022-11-29 14:54:46 +0100

    add more componets

--- Statistics.py
+++ Statistics.py
@@ -10,7 +10,7 @@ class Statistics:
     def __init__(self) -> None:
         self.api = c_api.Api()
         self.allCoinData = self.getAllCoinData()
-        self.statisticData = {"AVG": [], "MIN": [], "MAX": [], "SD": [], "Q1": [], "Q2": [], "Q3": [],"IQR": [],"RNG - low" : [], "RNG - high" : []}
+        self.statisticData = {"AVG": [], "MIN": [], "MAX": [], "SD": [], "Q1": [], "Q2": [], "Q3": [],"IQR": [],"RNG - low" : [], "RNG - high" : [], "UPS": [], "DOWNS": [], "LUPS": [],"LDWN": [],}
 
         # create statistics
         self.getStatistics()
@@ -45,11 +45,49 @@ class Statistics:
             self.statisticData["IQR"].append(self.calculateAverage(IQR))
             self.statisticData["RNG - low"].append(self.calculateAverage(min(coinData)))
             self.statisticData["RNG - high"].append(self.calculateAverage(max(coinData)))
+            self.statisticData["UPS"].append(self.calculateUps(coinData))
+            self.statisticData["DOWNS"].append(self.calculateDowns(coinData))
+            self.statisticData["LUPS"].append(self.calculateLongestUp(coinData))
+            self.statisticData["LDWN"].append(self.calculateLongestDown(coinData))
 
 
 
     def calculateAverage(self, coinData: list):
         return np.average(coinData)
+
+    def calculateUps(self, coinData: list):
+        return len([coinData[x] for x in range(0, len(coinData)) if coinData[x] > coinData[x-1]])
+
+    def calculateDowns(self, coinData: list):
+        return len([coinData[x] for x in range(0, len(coinData)) if coinData[x] < coinData[-1]])
+
+    def calculateLongestUp(self, coinData: list):
+
+        longestUp = 0
+        currentSequence = 0
+
+        for x in range(0, len(coinData)):
+            if coinData[x] > coinData[x-1]:
+                currentSequence += 1
+            else:
+                if currentSequence > longestUp:
+                    longestUp = currentSequence
+                currentSequence = 0
+        return longestUp
+
+    def calculateLongestDown(self, coinData: list):
+
+        longestDown = 0
+        currentSequence = 0
+
+        for x in range(0, len(coinData)):
+            if coinData[x] < coinData[x-1]:
+                currentSequence += 1
+            else:
+                if currentSequence > longestDown:
+                    longestDown = currentSequence
+                currentSequence = 0
+        return longestDown
 
     def dataFrame(self):
         df = pd.DataFrame(self.statisticData, index=[x["symbol"] for x in self.api.getCoins()])
