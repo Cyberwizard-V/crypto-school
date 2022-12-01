27ab42d0e62b9c6c074c6ef91d2d8f339b88f5ae
Kroonsz <Kroonszq@gmail.com>
2022-12-01 10:17:30 +0100

    create structure for phase 2

---
+++ phase2/Investor.py
@@ -0,0 +1,10 @@
+
+class Investor:
+    def __init__(self) -> None:
+        self.money = 1000000
+
+    def buyStock(self):
+        pass
+
+    def sellStock(self):
+        pass
---
+++ phase2/program.py
@@ -0,0 +1,14 @@
+import Investor as inv
+
+def main():
+
+    # create an object for every person
+    investors = {}
+    for person in ["Alice", "Bob", "Carol", "Dave", "Eve", "Frank"]:
+        investors[person] = inv.Investor()
+
+    
+    
+
+if __name__ == "__main__":
+    main()
