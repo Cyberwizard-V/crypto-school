37857e02f788605d92d673ece6cd44efa186f289
Kroonsz <Kroonszq@gmail.com>
2022-12-01 10:46:20 +0100

    Create investor class

--- phase2/Investor.py
+++ phase2/Investor.py
@@ -1,10 +1,17 @@
 
 class Investor:
-    def __init__(self) -> None:
+    def __init__(self, coin) -> None:
         self.money = 1000000
+        self.currency = {"coin": coin, "amount": 0} 
 
-    def buyStock(self):
-        pass
+    def buyStock(self, stockRate: float) -> None:
+        """ Buy stock rate with all the money """
+        self.currency["amount"] = int(self.money / stockRate)
+        self.money =- self.money
+        return
 
-    def sellStock(self):
-        pass
+    def sellStock(self, stockRate: float) -> None:
+        """ Sell stock rate with all coins """
+        self.money += int(self.currency["amount"] * stockRate)
+        self.currency["amount"] = 0
+        return
--- phase2/program.py
+++ phase2/program.py
@@ -1,12 +1,17 @@
 import Investor as inv
 
 def main():
-
+    fakeCoin = {"SWA": [100,50,120,123,231,536,123]}
     # create an object for every person
-    investors = {}
-    for person in ["Alice", "Bob", "Carol", "Dave", "Eve", "Frank"]:
-        investors[person] = inv.Investor()
+    investors = {"Alice": "ALB", "Bob": "BHA", "Carol": "CAS", "Dave": "DUB", "Eve": "ELG", "Frank": "FAW"}
+    for person in investors:
+        investors[person] = inv.Investor(investors[person])
 
+    investors["Alice"].buyStock(100)
+    print(investors["Alice"].currency["amount"])
+
+    investors["Alice"].sellStock(150)
+    print(investors["Alice"].money)
     
     
 
