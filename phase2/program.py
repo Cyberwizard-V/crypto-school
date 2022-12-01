06f9391a67f7f9721b18593e3581cc16e383efff
Kroonsz <Kroonszq@gmail.com>
2022-12-01 10:53:11 +0100

    Create if statements for every investor

--- phase2/program.py
+++ phase2/program.py
@@ -2,16 +2,39 @@ import Investor as inv
 
 def main():
     fakeCoin = {"SWA": [100,50,120,123,231,536,123]}
+
     # create an object for every person
     investors = {"Alice": "ALB", "Bob": "BHA", "Carol": "CAS", "Dave": "DUB", "Eve": "ELG", "Frank": "FAW"}
     for person in investors:
         investors[person] = inv.Investor(investors[person])
 
-    investors["Alice"].buyStock(100)
-    print(investors["Alice"].currency["amount"])
+    # loop trough the objects
+    for person in investors:
 
-    investors["Alice"].sellStock(150)
-    print(investors["Alice"].money)
+        if person == "Alice":
+            # BUY : when stock rate < 1500 SELL : when stock rate > 1600
+            pass
+
+        elif person == "Bob":
+            # BUY : when stock rate < 1000 SELL : when stock rate > 1100
+            pass
+
+        elif person == "Carol":
+            # BUY : when stock rate reaches valley (how do you detect a valley?) SELL : when stock rate reaches top (how do you detect a top?)
+            pass
+
+        elif person == "Dave":
+            # BUY : after three days of decreasing rates SELL : after three days of increasing rates
+            pass
+
+        elif person == "Eve":
+            # BUY : on day 1, 11, 21, 31, 41 , etc SELL : on day 5, 15, 25, 35, 45, etc
+            pass
+
+        elif person == "Frank":
+            # BUY : at day 1 and repeat: SELL : after an increase of 20% BUY again: after a decrease of 20%
+            pass
+
     
     
 
