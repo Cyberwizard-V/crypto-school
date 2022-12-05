1f926b6fbb2a7346cc2f96b01d757dd907b18961
Kroonsz <Kroonszq@gmail.com>
2022-12-05 21:53:25 +0100

    masterminde bot

--- Api.py
+++ Api.py
@@ -1,5 +1,6 @@
 import requests
 import pprint
+import json
 
 class Api:
     def __init__(self, API_KEY = "Dxi1TAblGX6Z6v59"):
@@ -23,6 +24,28 @@ class Api:
 
     def getSingleCoin(self, coinName) -> int:
         data = []
-        response = response = requests.get(f"https://api.basecampcrypto.nl/v1/coin/{coinName}/history?key=" + self.API_KEY)
+        response = requests.get(f"https://api.basecampcrypto.nl/v1/coin/{coinName}/history?key=" + self.API_KEY)
         data.append(response.json())
         return data
+
+    def getCurrentCoinValue(self, coinName):
+        response = requests.get(f"https://api.basecampcrypto.nl/v1/coin/{coinName}?key={self.API_KEY}")
+        return response.json()
+    
+    def buyCoin(self, coinName, cash):
+        data = {"amount": cash}
+        response = requests.post(f"https://api.basecampcrypto.nl/v1/coin/{coinName}/buy?key={self.API_KEY}", data=json.dumps(data))
+        print(response)
+    
+    def sellCoin(self, coinName, amount):
+        data = {"amount": amount}
+        response = requests.post(f"https://api.basecampcrypto.nl/v1/coin/{coinName}/sell?key={self.API_KEY}", data=json.dumps(data))
+        print(response)
+
+    def getTeamInfo(self):
+        response = requests.get(f"https://api.basecampcrypto.nl/v1/team?key={self.API_KEY}")
+        return response.json()
+
+    def getLastOrder(self):
+        response = requests.get(f"https://api.basecampcrypto.nl/v1/orders?key={self.API_KEY}")
+        return response.json()[-1]
--- phase4/tradingbot.py
+++ phase4/tradingbot.py
@@ -1,31 +1,38 @@
-import os, sys
+import os, sys, time
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import Api as c_api
 
 
-
-def
+cash = None
+coins = None
 
 def main():
-    pass
+    global cash
+    global coins
 
     # Buy stock < 10% and Sell stock > 10%
     # If after 10 days nothing happend it will change to 5% for once
 
     # Get coin history
     api = c_api.Api()
-    data = api.getSingleCoin("ALB")
-    coinData = [i["value"] for x in data for i in x['history']]
-
-    # Up - Sell
-    percentage = 0.10
-    if (coinData[-1] * (1 + percentage) ) >= coinData[-2]:
-        pass
-    # Dip - Buy
-    elif (coinData[-1] * (1 - percentage) ) <= coinData[-2]:
-        pass
 
 
+    while True:
+        data = api.getSingleCoin("ELG")
+        coinHistory = [i["value"] for x in data for i in x['history']]
+        currentRate = api.getCurrentCoinValue("ELG")
+
+        # Up - Sell
+        percentage = 0.10
+        if (currentRate["value"] * (1 + percentage) ) >= coinHistory[-2]:
+            api.sellCoin("ELG", coins)
+        # Dip - Buy
+        elif (currentRate["value"] * (1 - percentage) ) <= coinHistory[-2]:
+            cash = api.getTeamInfo()["cash"]
+            api.buyCoin("ELG", cash)
+            coins = api.getLastOrder()["quantity"]
+            
+        time.sleep(20)
 
 
 if __name__ == "__main__":
